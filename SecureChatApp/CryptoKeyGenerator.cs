aa1fa47f92f5213430ec6bf167616745a7657481
StefanoCuypers <stefano.cuypers@student.pxl.be>
2019-03-14 20:57:39 +0100

    Initial commit

---
+++ SecureChatApp/CryptoKeyGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Windows;
+
+namespace SecureChatApp
+{
+    class CryptoKeyGenerator
+    {
+        public string generateAESKey()
+        {
+            AesManaged aesEncryption = new AesManaged();
+            aesEncryption.KeySize = 256;
+            aesEncryption.GenerateIV();
+            string ivStr = Convert.ToBase64String(aesEncryption.IV);
+            aesEncryption.GenerateKey();
+            string keyStr = Convert.ToBase64String(aesEncryption.Key);
+            string completeKey = ivStr + "," + keyStr;
+            return completeKey;
+        }
+
+        public void generateRSAKey()
+        {
+     
+
+            //stream to save the keys
+            FileStream fs = null;
+            StreamWriter sw = null;
+
+            //create RSA provider
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(4096);
+            try
+            {
+                //save private key
+                fs = new FileStream(Path.Combine(Environment.CurrentDirectory, "Private_A.txt"), FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.Write(rsa.ToXmlString(true));
+                sw.Flush();
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                if (fs != null) fs.Close();
+            }
+
+            try
+            {
+                //save public key
+                fs = new FileStream(Path.Combine(Environment.CurrentDirectory, "Public_A.txt"), FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.Write(rsa.ToXmlString(false));
+                sw.Flush();
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                if (fs != null) fs.Close();
+            }
+            rsa.Clear();
+        }
+        
+    }
+}
